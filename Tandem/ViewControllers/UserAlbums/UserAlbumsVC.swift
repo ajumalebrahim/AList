8a5ed75ff5c380df3c81d945f455e70a0884a9b3
ajumalebrahim <ajumal.ebrahim@gmail.com>
2019-04-08 19:18:39 +0300

    Grouped properly

---
+++ Tandem/ViewControllers/UserAlbums/UserAlbumsVC.swift
@@ -0,0 +1,30 @@
+//
+//  UserAlbumsVC.swift
+//  Tandem
+//
+//  Created by Ayman Banani on 4/8/19.
+//  Copyright © 2019 Ajumal Ebrahim. All rights reserved.
+//
+
+import UIKit
+
+class UserAlbumsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
