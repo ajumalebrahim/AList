5d364ac8a3194d37e4db07ba4ea60bd2af292b0e
ajumalebrahim <ajumal.ebrahim@gmail.com>
2019-04-08 20:04:17 +0300

    Album cel configured

--- Tandem/ViewControllers/UserAlbums/SubView/AlbumCell.swift
+++ Tandem/ViewControllers/UserAlbums/SubView/AlbumCell.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import SDWebImage
 
 class AlbumCell: UICollectionViewCell {
-
+    // MARK: - Outlets
+    @IBOutlet private weak var lblImgName: UILabel!
+    @IBOutlet private weak var imgvw: UIImageView!
+    
+    // MARK: - Variables
+    var album: Album? {
+        didSet {
+            config()
+        }
+    }
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static var nib: UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    // MARK: - override
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    private func config() {
+        self.imgvw.sd_setImage(with: URL(string:self.album?.url ?? ""))
+        self.lblImgName.text = "\(self.album?.id ?? 0 )"
+    }
+    
 
 }
